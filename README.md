d38b10b02a3d71e3a20137f9edfad9154d61424c
mef8dd@virginia.edu <mef8dd@virginia.edu>
2020-02-11 08:30:08 -0500

    added Procfile and requirements.txt

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # project-102-qtees
+
+# This is the README File for CS 3240 QTees Quick Tutor Project
+
+
--- mysite/settings.py
+++ mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap4',
+    'django_forms_bootstrap',
 ]
 
 MIDDLEWARE = [
