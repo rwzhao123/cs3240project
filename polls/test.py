d5723a5fb18d997b3896e5474cf42a06e6f819fa
Brennan McGowan <mcgowan.brennan@gmail.com>
2020-02-18 18:35:44 -0500

    Example test file

---
+++ polls/test.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+from .models import Student
+
+class StudentTestCase(TestCase):
+    def setUp(self):
+        Student.objects.create(student_first_name="Brenna", student_last_name="McGowan", student_email="mcgowan.brennan@gmail.com")
