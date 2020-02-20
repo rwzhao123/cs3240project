9f4b2ccb40341ec2ba76627ceb92afda546a659d
Brennan McGowan <mcgowan.brennan@gmail.com>
2020-02-20 13:05:22 -0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'kddgyphr'),
+        'NAME': 'kddgyphr',
         'USER': 'kddgyphr',
         'PASSWORD': 'VAwGR4cdvbUtY1x8o4czPyU-NACRxZFH',
         'HOST': 'rajje.db.elephantsql.com',
