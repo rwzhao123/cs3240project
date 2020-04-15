b9a91e5ef9dbe14698527d624eb99ec8237bdfdb
Brennan McGowan <mcgowan.brennan@gmail.com>
2020-04-15 11:16:28 -0400

    Update views.py

--- polls/views.py
+++ polls/views.py
@@ -79,10 +79,6 @@ def index(request):
 def chat(request):
     return render(request, 'polls/chat.html')
 
-def room(request, room_name):
-    return render(request, 'polls/room.html', {
-        'room_name': room_name
-    })
 
 from django.views.generic import ListView
 
@@ -192,3 +188,34 @@ class AllStudentsView(generic.ListView):
     template_name = 'polls/tutor_match.html'
     def get_queryset(self):
         return Student.objects.all()
+    
+def new_room(request):
+    """
+    Randomly create a new room, and redirect to it.
+    """
+    new_room = None
+    while not new_room:
+        with transaction.atomic():
+            label = haikunator.haikunate()
+            if Room.objects.filter(label=label).exists():
+                continue
+            new_room = Room.objects.create(label=label)
+    return redirect(chat_room, label=label)
+
+def chat_room(request, label):
+    """
+    Room view - show the room, with latest messages.
+    The template for this view has the WebSocket business to send and stream
+    messages, so see the template for where the magic happens.
+    """
+    # If the room with the given label doesn't exist, automatically create it
+    # upon first visit (a la etherpad).
+    room, created = Room.objects.get_or_create(label=label)
+
+    # We want to show the last 50 messages, ordered most-recent-last
+    messages = reversed(room.messages.order_by('-timestamp')[:50])
+
+    return render(request, "chat/room.html", {
+        'room': room,
+        'messages': messages,
+    })
