04d3984734b096f794c5bce3725b11e155334a64
Brennan McGowan <mcgowan.brennan@gmail.com>
2020-04-15 12:52:04 -0400

    Update asgi.py

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -1,5 +1,12 @@
+"""
+ASGI entrypoint. Configures Django and then runs the application
+defined in the ASGI_APPLICATION setting.
+"""
+
 import os
-import channels.asgi
+import django
+from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-channel_layer = channels.asgi.get_channel_layer()
+django.setup()
+application = get_default_application()
