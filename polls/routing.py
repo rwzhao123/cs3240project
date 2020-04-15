5d6ea8e8c90ab0fdb92410c962bba74b69a9932f
Brennan McGowan <mcgowan.brennan@gmail.com>
2020-04-15 11:35:36 -0400

    Update routing.py

--- polls/routing.py
+++ polls/routing.py
@@ -1,7 +1,14 @@
-from django.urls import re_path
-
+from channels.staticfiles import StaticFilesConsumer
 from . import consumers
 
-websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer),
-]
+channel_routing = {
+    # This makes Django serve static files from settings.STATIC_URL, similar
+    # to django.views.static.serve. This isn't ideal (not exactly production
+    # quality) but it works for a minimal example.
+    'http.request': StaticFilesConsumer(),
+
+    # Wire up websocket channels to our consumers:
+    'websocket.connect': consumers.ws_connect,
+    'websocket.receive': consumers.ws_receive,
+    'websocket.disconnect': consumers.ws_disconnect,
+}
